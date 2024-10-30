d332015af195ad31be452107a1ddd7997e85edab
Leonardo <leo.silvestri.04@gmail.com>
2024-10-30 10:55:52 +0100

    Update favicon

--- app/(pages)/[lang]/layout.tsx
+++ app/(pages)/[lang]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import {siteConfig} from "@/utils/config";
+import favicon from "@/public/favicon.ico";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -26,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="it">
+      <head>
+        <link rel="icon" href={favicon.src} sizes="" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
