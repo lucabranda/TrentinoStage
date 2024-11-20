be0d9848095d9377131148f0159eed808c192c59
Leonardo <leo.silvestri.04@gmail.com>
2024-11-20 16:50:54 +0100

    Restyle dashboard layout

--- app/(pages)/[lang]/dashboard/company/page.tsx
+++ app/(pages)/[lang]/dashboard/company/page.tsx
@@ -5,7 +5,6 @@ import OffersSection from '@/components/dashboard/company/OffersSection';
 import { TeamOutlined, RocketOutlined, InboxOutlined, LogoutOutlined } from '@ant-design/icons';
 import { Content } from '@/components/Layout/Layout';
 import DashboardLayout from '@/components/Layout/DashboardLayout';  // Assuming layout is here
-
 export default async function CompanyPage({ params }: any) {
   const messages = await getMessages((await params).lang);
   const itemsSidebar = [
@@ -29,6 +28,7 @@ export default async function CompanyPage({ params }: any) {
       icon: <LogoutOutlined />,
       label: <>{messages["logout"] || "Logout"}</>,
     },
+    
   ];
 
   return (
--- app/(pages)/[lang]/dashboard/user/page.tsx
+++ app/(pages)/[lang]/dashboard/user/page.tsx
@@ -45,7 +45,7 @@ export default async function UserPage({params}: any) {
            {messages[""] || ""}
            </>
          ),
-       },
+       }
      ];
   return(
     <DashboardLayout params={params} itemsSidebar={items}>
